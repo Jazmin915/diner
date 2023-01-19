86fdff9740acb900cac069b7f4c1f33410f8d751
Jazmin Gonzalez <gonzalez.jazmin@student.greenriver.edu>
2023-01-19 13:02:14 -0500

    controller and home page view added

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+// This is my CONTROLLER for the hello project
+
+// Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+// Require the autoload file
+require_once('vendor/autoload.php');
+
+// Create an instance of the Base class (instantiate F3 base class)
+$f3 = Base::instance();
+// Java equivalent -> Base f3 = new Base();
+
+// Define a default route ("home page" for diner/328)
+$f3->route('GET /', function () {
+
+    //Instantiate a view
+    //Template is a class in the fat free framework
+    $view = new Template();
+    echo $view->render("views/diner-home.html");
+});
+
+
+
+//Run Fat-free
+$f3->run();
+// Java -> f3.run();
